a8192ec416caa91ffb1be82da4cbd7c819c426d6
Piotr Adamowicz <piotr.adamowicz@gmail.com>
2023-01-13 20:25:23 +0100

    Move context provider to index. Leave only router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import ScrollWatchContextProvider from './context/ScrollWatchContext';
 import router from './router';
 
 function App() {
   return (
-    <>
-      <ScrollWatchContextProvider>
-        <RouterProvider router={router} />
-      </ScrollWatchContextProvider>
-    </>
+    <RouterProvider router={router} />
   );
 }
 
