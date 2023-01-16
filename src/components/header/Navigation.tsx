f53ee5285cfaca6e42e734761fa8429b3871bfa9
Piotr Adamowicz <piotr.adamowicz@gmail.com>
2023-01-16 20:03:55 +0100

    Responsive navigation. Menu toggle.

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { IoMenu, IoClose } from 'react-icons/io5';
 import './Navigation.css';
 
 const Navigation = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = useCallback(
+    () => {
+      setMenuOpened(prevState => !prevState);
+    }, [setMenuOpened],
+  );
+
   return (
     <div className="Navigation-container">
       <nav className="Navigation">
         <div>Piotr Adamowicz</div>
-        <ul className="Navigation-links">
+        <ul className={menuOpened ? 'Navigation-links Navigation-links_opened' : 'Navigation-links'}>
+          <li className="Navigation-close" onClick={toggleMenu}><IoClose /></li>
           <li>Home</li>
           <li>About</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>
+        <div className="Navigation-open" onClick={toggleMenu}><IoMenu /></div>
       </nav>
     </div>
   );
