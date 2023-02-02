47e009392f17df438822f24b6002883b55b5363e
Piotr Adamowicz <piotr.adamowicz@gmail.com>
2023-02-02 15:15:57 +0100

    Test deployment process

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
         on.</p>
       <p>This particular page was done with React.js and some CSS.</p>
       <p>To contact me please visit my <a href="https://www.linkedin.com/in/piotradamowicz/" target="_blank"
-        rel="noopener noreferrer">LinkedIn profile</a></p>
+        rel="noopener noreferrer">LinkedIn profile</a>.</p>
       <div style={{ visibility: 'hidden' }}>
         <p>long...</p>
         <p>long...</p>
